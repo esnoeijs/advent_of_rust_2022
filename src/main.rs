6767f49eaa7e69ecb0e583049ed00ddd5a35c9ba
Emma Snoeijs <emmasnoeijs@gmail.com>
2023-07-23 17:35:54 +0200

    day4

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod day1;
 mod day2;
 mod day3;
+mod day4;
 mod file;
 
 use std::env;
@@ -15,6 +16,7 @@ fn main() {
         1 => day1::solution(filename),
         2 => day2::solution(filename),
         3 => day3::solution(filename),
+        4 => day4::solution(filename),
         _ => panic!("unknown day number"),
     }
 }
